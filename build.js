30d829bd817325460d144259e2d9ee76b2d4f309
chenjk110 <chenjk110@qq.com>
2021-07-19 01:13:50 +0800

    disable emit sourceMap

--- build.js
+++ build.js
@@ -2,14 +2,20 @@ const child_process = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
-child_process.execSync('rm -rf ./dist')
-child_process.execSync('tsc')
-child_process.execSync('cp README.md surprise.jpeg ./dist/')
+try {
+  child_process.execSync('rm -rf ./dist')
+  child_process.execSync('tsc')
+  child_process.execSync('cp README.md surprise.jpeg ./dist/')
 
-const pkg = require('./package.json')
+  const pkg = require('./package.json')
 
-Reflect.set(pkg, 'main', 'index.js')
-Reflect.deleteProperty(pkg, 'scripts')
-Reflect.deleteProperty(pkg, 'devDependencies')
+  Reflect.set(pkg, 'main', 'index.js')
+  Reflect.deleteProperty(pkg, 'scripts')
+  Reflect.deleteProperty(pkg, 'devDependencies')
 
-fs.writeFileSync('./dist/package.json', JSON.stringify(pkg, null, 2))
+  fs.writeFileSync('./dist/package.json', JSON.stringify(pkg, null, 2))
+} catch (err) {
+
+  console.error(err.message)
+  console.error(err.stdout.toString())
+}
